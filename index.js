762853fc4693c5e18c2b5c20f0c07ac61a3d16c7
Ryan Tablada <ryan.tablada@gmail.com>
2015-11-11 20:28:30 -0600

    Simple RDIO Top Charts

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').load();
+var clientId = process.env.RDIO_ID;
+var clientSecret = process.env.RDIO_SECRET;
+
+var Rdio = require('rdio')({
+  rdio: {
+    clientId,
+    clientSecret,
+  },
+});
+var express = require('express');
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  var rdio = new Rdio();
+
+  rdio.getClientToken(function(err) {
+    if (err) {
+      return console.log('err', err);
+    }
+
+    rdio.request({method: 'getTopCharts', type: 'Track'}, false, function(err, data) {
+      if (err) {
+        return console.log('err: ', err);
+      }
+
+      res.send(data);
+    });
+  });
+});
+
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
